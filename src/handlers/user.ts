ff0a284a3dfb1531895b69cebbdd42d0495c33b1
Miguel Cobian <miguelcobian99@gmail.com>
2024-07-23 16:20:12 -0600

    Feat. get all users endpoint

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -10,26 +10,38 @@ export const registerUser = async (req: Request, res: Response) => {
 
     try {
         // Avoid duplicates
-        const userExists = await User.findOne(email);
+        const userExists = await User.findOne({ where: { email } });
 
         if (userExists) {
             const error = new Error('User is already registered');
             return res.status(409).json({ error: error.message });
         }
 
-        const user = await User.create(req.body);
-
-        user.password = await hashPassword(password);
+        // Create user and update password hashed
+        const user = await User.create({
+            ...req.body,
+            password: await hashPassword(password),
+        });
 
         // Generate token
-        const token = new Token();
-        token.token = generateToken();
-        token.userId = user.id;
-
-        await Promise.allSettled([user.save(), token.save()]);
+        await Token.create({
+            token: generateToken(),
+            userId: user.id,
+        });
 
         res.send('Account registered successfully');
     } catch (error) {
         res.status(500).json({ error: 'Something went wrong' });
+    }
+}
+
+export const getUsers = async (req: Request, res: Response) => {
+    try {
+        const users = await User.findAll();
+
+        res.json({ data: users });
+
+    } catch (error) {
+        console.log(error);
     }
 }
--- src/router/usersRouter.ts
+++ src/router/usersRouter.ts
@@ -1,15 +1,12 @@
 import { Router } from "express";
-import { registerUser } from "../handlers/user";
+import { getUsers, registerUser } from "../handlers/user";
 
 
 const usersRouter = Router();
 
 usersRouter.post('/', registerUser);
 
-// Main
-usersRouter.get('/', (req, res) => {
-    res.json('All the users');
-});
+usersRouter.get('/', getUsers);
 
 
 
