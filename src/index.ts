a0e5111fc9d382182c4f506d156ed35c826343be
Miguel Cobian <miguelcobian99@gmail.com>
2024-07-29 17:37:57 -0600

    Fix. Remove port env variable

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import server from "./server";
 import colors from 'colors';
 
-const port = process.env.PORT || 4000;
+const port = 4000;
 
 server.listen(port, () => {
     console.log(colors.cyan.bold(`REST API on port ${process.env.PORT}`));
